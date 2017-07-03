fee91624aa06cb094181b66018bc494114e327d3
chenzehua <872990547@qq.com>
2017-07-03 15:59:45 +0800

    header组件详情弹页层完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,5 +1,5 @@
 <template>
-     <div class="header">
+     <div class="header" ref="header">
        <div class="content-wrapper">
          <div class="avatar">
            <img :src="seller.avatar" width="64" height="64" alt=""/>
@@ -17,30 +17,82 @@
              <span class="text">{{seller.supports[0]['description']}}</span>
            </div>
          </div>
-         <div v-if="seller.supports" class="support-count">
+         <div @click="showDetail" v-if="seller.supports" class="support-count">
            <span class="count">{{seller.supports.length}}</span>
            <i class="icon-keyboard_arrow_right"></i>
          </div>
        </div>
-       <div class="bulletin-wrapper">
+       <div class="bulletin-wrapper" @click="showDetail">
          <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
          <i class="icon-keyboard_arrow_right"></i>
        </div>
        <div class="background">
          <img :src="seller.avatar" width="100%" height="100%" alt=""/>
        </div>
+       <transition name="fade">
+         <div class="detail" v-show="detailShow">
+           <div class="detail-wrapper clearfix">
+             <div class="detail-main">
+               <h1 class="name">{{ seller.name }}</h1>
+               <div class="star-wrapper">
+                 <star :size="48" :score="seller.score"></star>
+               </div>
+               <div class="title">
+                 <div class="line"></div>
+                 <div class="text">优惠信息</div>
+                 <div class="line"></div>
+               </div>
+               <ul v-if="seller.supports" class="supports">
+                 <li class="support-item" v-for="(item,index) in seller.supports">
+                   <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                   <span class="text">{{ seller.supports[index].description }}</span>
+                 </li>
+               </ul>
+               <div class="title">
+                 <div class="line"></div>
+                 <div class="text">商家公告</div>
+                 <div class="line"></div>
+               </div>
+               <div class="bulletin">
+                 <p class="content">{{ seller.bulletin }}</p>
+               </div>
+             </div>
+           </div>
+           <div class="detail-close">
+             <i class="icon-close" @click="closeDetail"></i>
+           </div>
+         </div>
+       </transition>
      </div>
 </template>
 <script type="text/ecmascript-6">
+  import star from "../star/star.vue";
     export default {
         data () {
             return {
+              detailShow: false
             }
         },
         created() {
           this.classMap = ['decrease','discount','special','invoice','guarantee'];
+
         },
-        props:['seller'],
+        props:{
+          seller:{
+            type: Object
+          }
+        },
+        methods: {
+          showDetail(){
+              this.detailShow = true;
+          },
+          closeDetail(){
+            this.detailShow = false;
+          }
+        },
+        components:{
+          star
+        }
 
 
     }
@@ -50,6 +102,7 @@
   .header{
     position: relative;
     color: #fff;
+    overflow: hidden;
     background-color: rgba(7,17,27,0.5);
   .content-wrapper{
       position: relative;
@@ -107,13 +160,13 @@
             }
             &.guarantee {
              .bg-image('guarantee_1')
-             }
+            }
             &.invoice {
              .bg-image('invoice_1')
-             }
+            }
             &.special {
              .bg-image('special_1')
-             }
+            }
           }
           .text{
             line-height: 12px;
@@ -182,6 +235,116 @@
       height: 100%;
       z-index: -1;
       filter: blur(10px);
+    }
+    .detail{
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background-color: rgba(7, 17, 27, 0.8);
+      backdrop-filter: blur(20px);
+      &.fade-enter-active,&.fade-leave-active{
+        transition: all 1s;
+      }
+      &.fade-enter,&.fade-leave-active{
+        opacity: 0;
+      }
+      .detail-wrapper{
+        width: 100%;
+        min-height: 100%;
+        .detail-main{
+          margin-top: 64px;
+          padding-bottom: 64px;
+          .name{
+            line-height: 16px;
+            font-size: 16px;
+            font-weight: 700;
+            text-align: center;
+          }
+          .star-wrapper{
+            margin-top: 18px;
+            padding: 2px 0;
+            text-align: center;
+          }
+          .title{
+            display: flex;
+            width: 80%;
+            margin: 28px auto 24px auto;
+            .line{
+              flex: 1;
+              position: relative;
+              top: -6px;
+              border-bottom: 1px solid rgba(255,255,255,0.2);
+            }
+            .text{
+              padding: 0 12px;
+              font-size: 14px;
+              font-weight: 700;
+            }
+          }
+          .supports{
+            width: 80%;
+            margin: 0 auto;
+            .support-item{
+              padding: 0 12px;
+              margin-bottom: 12px;
+              font-size: 0;
+              &:last-child{
+                 margin-bottom: 0;
+              }
+              .icon{
+                display: inline-block;
+                width: 16px;
+                height: 16px;
+                vertical-align: top;
+                margin-right: 6px;
+                -webkit-background-size: 16px 16px;
+                background-size: 16px 16px;
+                background-repeat: no-repeat;
+                &.decrease {
+                 .bg-image('decrease_2')
+                }
+                &.discount {
+                 .bg-image('discount_2')
+                }
+                &.guarantee {
+                 .bg-image('guarantee_2')
+                }
+                &.invoice {
+                 .bg-image('invoice_2')
+                }
+                &.special {
+                 .bg-image('special_2')
+                }
+              }
+              .text{
+                line-height: 16px;
+                font-size: 12px;
+              }
+            }
+          }
+          .bulletin{
+            width: 80%;
+            margin: 0 auto;
+            .content{
+              padding: 0 12px;
+              line-height: 24px;
+              font-size: 12px;
+            }
+          }
+        }
+      }
+      .detail-close{
+        position: relative;
+        width: 32px;
+        height: 32px;
+        margin: -64px auto 0 auto;
+        clear: both;
+        font-size: 32px;
+      }
     }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue';
 import App from './App';
 
-import goods from './components/goods';
-import ratings from './components/ratings';
-import seller from './components/seller';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
 
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
